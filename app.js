cd99c338ab5c787f22df875c698162557923c50a
Amjadshaik14 <amjadshaik477@gmail.com>
2026-01-14 18:53:44 +0530

    Added Get method with a param

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
 
+// GET Method to get all tours
 app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "Success",
@@ -17,6 +18,7 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
+// POST Method to create a tour
 app.post("/api/v1/tours", (req, res) => {
   console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
@@ -35,6 +37,27 @@ app.post("/api/v1/tours", (req, res) => {
       });
     },
   );
+});
+
+// GET method with an id parameter to get a specific tour
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  console.log(req.params); // {id: "4"}
+  const id = req.params.id * 1; // This is to convert the id from a string to an int.
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: "Failed",
+      message: "Invalid ID",
+    });
+  }
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: "Success",
+    data: {
+      // tour: tours[req.params.id], // you can also do this assuming that all the data is in ascending order sorted by id.
+      tour,
+    },
+  });
 });
 
 app.listen(PORT, () => {
