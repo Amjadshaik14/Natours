ee423698603c2bb47c3867937a255a06b5d5dece
Amjadshaik14 <amjadshaik477@gmail.com>
2026-01-14 19:43:50 +0530

    Refactored the code

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const tours = JSON.parse(
 );
 
 // GET Method to get all tours
-app.get("/api/v1/tours", (req, res) => {
+
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: "Success",
     result: tours.length,
@@ -16,10 +17,28 @@ app.get("/api/v1/tours", (req, res) => {
       tours,
     },
   });
-});
+};
 
-// POST Method to create a tour
-app.post("/api/v1/tours", (req, res) => {
+const getTour = (req, res) => {
+  console.log(req.params); // {id: "4"}
+  const id = req.params.id * 1; // This is to convert the id from a string to an int.
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: "Failed",
+      message: "Invalid ID",
+    });
+  }
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: "Success",
+    data: {
+      // tour: tours[req.params.id], // you can also do this assuming that all the data is in ascending order sorted by id.
+      tour,
+    },
+  });
+};
+
+const createTour = (req, res) => {
   console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -37,32 +56,9 @@ app.post("/api/v1/tours", (req, res) => {
       });
     },
   );
-});
+};
 
-// GET method with an id parameter to get a specific tour
-
-app.get("/api/v1/tours/:id", (req, res) => {
-  console.log(req.params); // {id: "4"}
-  const id = req.params.id * 1; // This is to convert the id from a string to an int.
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: "Failed",
-      message: "Invalid ID",
-    });
-  }
-  const tour = tours.find((el) => el.id === id);
-  res.status(200).json({
-    status: "Success",
-    data: {
-      // tour: tours[req.params.id], // you can also do this assuming that all the data is in ascending order sorted by id.
-      tour,
-    },
-  });
-});
-
-// PATCH method to update a specific tour
-
-app.patch("/api/v1/tours/:id", (req, res) => {
+const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: "Failed",
@@ -74,10 +70,9 @@ app.patch("/api/v1/tours/:id", (req, res) => {
     status: "Success",
     tour: "<---Your updated tour here--->",
   });
-});
+};
 
-// DELETE method to delete a tour
-app.delete("/api/v1/tours/:id", (req, res) => {
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: "Failed",
@@ -89,7 +84,31 @@ app.delete("/api/v1/tours/:id", (req, res) => {
     status: "Success",
     data: null,
   });
-});
+};
+
+// app.get("/api/v1/tours", getAllTours);
+
+// POST Method to create a tour
+// app.post("/api/v1/tours", createTour);
+
+// GET method with an id parameter to get a specific tour
+
+// app.get("/api/v1/tours/:id", getTour);
+
+// PATCH method to update a specific tour
+
+// app.patch("/api/v1/tours/:id", updateTour);
+
+// DELETE method to delete a tour
+// app.delete("/api/v1/tours/:id", deleteTour);
+
+app.route("/api/v1/tours").get(getAllTours).post(createTour);
+app
+  .route("/api/v1/tours/:id")
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
 app.listen(PORT, () => {
   console.log(`Server started at port: ${PORT}`);
 });
